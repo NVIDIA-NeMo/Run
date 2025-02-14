9522da6fa0ae82d304f8f9a18ca8f8c5eb41f8a9
Amol Umbark <amolumbarkar@gmail.com>
2025-02-14 10:27:19 +0530

    Update zlib_json.py
    Signed-off-by: Amol Umbark <amolumbarkar@gmail.com>

--- src/nemo_run/core/serialization/zlib_json.py
+++ src/nemo_run/core/serialization/zlib_json.py
@@ -1,22 +1,9 @@
-# SPDX-FileCopyrightText: Copyright (c) 2024 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
-# SPDX-License-Identifier: Apache-2.0
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-# http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
 
 import base64
 import zlib
 from typing import Optional
-
+import os
+import json
 from fiddle._src import config
 from fiddle._src.experimental import serialization
 
@@ -29,16 +16,27 @@ class ZlibJSONSerializer:
         cfg: config.Buildable,
         pyref_policy: Optional[serialization.PyrefPolicy] = None,
     ) -> str:
-        return base64.urlsafe_b64encode(
-            zlib.compress(serialization.dump_json(cfg, pyref_policy).encode())
-        ).decode("ascii")
+        
+        #return base64.urlsafe_b64encode(
+        #    zlib.compress(serialization.dump_json(cfg, pyref_policy).encode())
+        #).decode("ascii")
+        # print("value:" , serialization.dump_json(cfg, pyref_policy))
+        return serialization.dump_json(cfg, pyref_policy)
 
     def deserialize(
         self,
         serialized: str,
         pyref_policy: Optional[serialization.PyrefPolicy] = None,
     ) -> config.Buildable:
-        return serialization.load_json(
-            zlib.decompress(base64.urlsafe_b64decode(serialized)).decode(),
-            pyref_policy=pyref_policy,
-        )
+        # print("serialized:", serialized)
+        if os.path.isfile(serialized):
+            with open(serialized, "r+") as f:
+                try:
+                    serialized_str = json.load(f)
+                except Exception:
+                    serialized_str = ""
+        else:
+            serialized_str = serialized
+        # print("serialized_str:", serialized_str)
+        return serialization.load_json(serialized_str,pyref_policy=pyref_policy)
+
