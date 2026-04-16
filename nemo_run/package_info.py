045e8b8aa4ead94b79df43d1ed4332b8389d0818
oliver könig <okoenig@nvidia.com>
2026-04-16 22:04:48 +0200

    Release 0.9.0
    Signed-off-by: oliver könig <okoenig@nvidia.com>

--- nemo_run/package_info.py
+++ nemo_run/package_info.py
@@ -13,7 +13,7 @@
 # limitations under the License.
 from packaging.version import Version
 
-__version__ = "0.9.0rc1"
+__version__ = "0.9.0"
 
 MAJOR = Version(__version__).major
 MINOR = Version(__version__).minor
