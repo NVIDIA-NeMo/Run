4d8015da80628511e538aeb3f78b689144fc7e5e
oliver könig <okoenig@nvidia.com>
2026-06-03 07:40:59 +0000

    fix: set PRE_RELEASE to empty string
    Signed-off-by: oliver könig <okoenig@nvidia.com>

--- nemo_run/package_info.py
+++ nemo_run/package_info.py
@@ -15,7 +15,7 @@
 MAJOR = 0
 MINOR = 11
 PATCH = 0
-PRE_RELEASE = "rc0.dev0"
+PRE_RELEASE = ""
 
 # Use the following formatting: (major, minor, patch, pre-release)
 VERSION = (MAJOR, MINOR, PATCH, PRE_RELEASE)
